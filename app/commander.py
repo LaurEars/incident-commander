65e4aba86730525a75e915fe61eb15b681817cc3
Laura <laura.rupprecht@zefr.com>
2016-06-09 13:24:32 -0700

    Add parsing for adding message

--- app/commander.py
+++ app/commander.py
@@ -1,6 +1,28 @@
+import re
+
 import rethinkdb as r
+
 
 class Commander:
 
     def process_message(self, message):
-        return "I got your message"
+        return self.parse_message(message)
+
+    def parse_message(self, message):
+        stripped_message = message.strip()
+        commander_match = re.match(r'commander\s*(.*)',
+                                   stripped_message,
+                                   flags=re.IGNORECASE)
+        if commander_match:
+            # parse message as incident commander message
+            task_match = re.match(r'add task\s*(.*)',
+                                  commander_match.groups()[0],
+                                  flags=re.I)
+            if task_match:
+                return self.add_task(task_match.groups()[0])
+            return 'no match for this command'
+
+    def add_task(self, task):
+        # add task to task list
+        print(task)
+        return 'Added task to list!'
