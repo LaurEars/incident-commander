d0c52c666538593d59e6224fedb2eb8a51137140
Laura <laura.rupprecht@zefr.com>
2016-06-09 14:35:07 -0700

    Add code for creating incidents

--- app/commander.py
+++ app/commander.py
@@ -34,15 +34,32 @@ class Commander:
                               stripped_message,
                               flags=re.IGNORECASE)
         if name_match:
+            commands = name_match.groups()[0]
             # parse message as incident commander message
-            task_match = re.match(r'add task\s*(.*)',
-                                  name_match.groups()[0],
-                                  flags=re.I)
+            task_match = re.match(r'add task\s*(.*)', commands, flags=re.I)
             if task_match:
                 return self.add_task(task_match.groups()[0])
+
+            create_incident = re.match(r'create[ -]incident\s*(.*)',
+                                       commands,
+                                       flags=re.I)
+            if create_incident:
+                # begin workflow for creating incident
+                return self.create_incident(create_incident.groups()[0])
             return 'no match for this command'
 
     def add_task(self, task):
-        # add task to task list
+        # todo: add task to task list
         print(task)
         return 'Added task to list!'
+
+    def create_incident(self, app_name):
+        # catches "for app-name" or "app-name"
+        current_app_name = re.match(r'(?:for ?)(.*)', app_name)
+        if not current_app_name:
+            return 'Hey, did you forget to include an application name'
+
+        # todo: make channel
+        # todo: say stuff in channel
+        # todo: push empty document to database
+        return 'Created incident!'
