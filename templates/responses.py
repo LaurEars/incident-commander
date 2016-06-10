7d878f1e146ab91ab05ada872c1e1b29d83e8e9d
Henry Jordan <henry.jordan@zefr.com>
2016-06-09 18:45:41 -0700

    more templating

--- templates/responses.py
+++ templates/responses.py
@@ -1,21 +1,24 @@
 from jinja2 import Template
 
 CREATE_INCIDENT = Template("""
-INCIDENT CREATED!
+*INCIDENT CREATED!*
+
+This incident is now being tracked and documented, but we still need your help!
 
 When you have a moment, please use these commands to provide more information:
-@commander set-title <title>
-@commander set-reporter <@reporter>
-....
-
+```
+@commander set title <title>
+@commander set reporter <@reporter>
+@commander set severity <1-3>
+@commander set description <description>
+```
 """)
-CREATE_INCIDENT_FAILED = Template("Hey, did you forget to include an application name?")
+CREATE_INCIDENT_FAILED = Template("Hey, did you forget to include an application name?")  # noqa
 
 SET = Template("Set *{{field}}* to `{{value}}`")
 GET = Template("*{{field}}*: {{value}}")
 
-GET_LIST = Template(
-"""
+GET_LIST = Template("""
 *{{field}}:*
 ```
 {% for val in value %}
